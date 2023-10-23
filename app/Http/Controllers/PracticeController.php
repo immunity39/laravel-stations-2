59244c0d9b235657f8dd01863f9db9d14855f07a
immunity39 <so_kaicyo@icloud.com>
2023-10-23 12:57:26 +0900

    laravel-station2

---
+++ app/Http/Controllers/PracticeController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class PracticeController extends Controller
+{
+    public function sample() {
+        return view('practice');
+    }
+
+    public function sample2() {
+        $test = 'practice2';
+        return view('practice2', ['testParam' => $test]);
+    }
+
+    public function sample3(){
+        $testParam = 'test';
+        return view('practice3', ['testP' => $testParam]);
+    }
+}
+?>
+
