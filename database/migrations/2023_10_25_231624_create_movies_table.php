2577125d080ea09a9d402c10ab4aa64f42da88b6
immunity39 <so_kaicyo@icloud.com>
2023-10-26 03:16:01 +0900

    station06

---
+++ database/migrations/2023_10_25_231624_create_movies_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMoviesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->id();
+            $table->text('title')->comment('映画タイトル');
+            $table->text('image_url')->comment('画像URL');
+            //$table->year('published_year')->comment('公開年');
+            //$table->text('description')->comment('説明');
+            //$table->boolean('is_showing')->comment('上映中かどうか');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movies');
+    }
+}
